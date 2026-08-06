153b6f287f2a4b1de9c2ef46ae137da000f32581
leonardosilva-ti <leonardo.silvati@outlook.com>
2026-08-06 18:22:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function getSelectedDateString() {
     return `${yyyy}-${mm}-${dd}`;
 }
 
-// Helpers de Conversão de Moedas (Máscara)
+// Helpers de Conversão de Moedass (Máscara)
 function parseCurrency(val) {
     if (!val) return 0;
     if (typeof val === 'number') return val;
